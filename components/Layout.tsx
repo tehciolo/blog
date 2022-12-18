f3750720e7c7d0ffd9c8cddce3b0f91886bf6a69
Cosmin Cioaclă <tehciolo@gmail.com>
2022-12-18 18:18:30 +0100

    post: holy grail layout

---
+++ components/Layout.tsx
@@ -0,0 +1,42 @@
+import { ComponentChildren } from "preact";
+import Container from "./Container.tsx";
+
+export default function Layout(props: { children: ComponentChildren }) {
+  const { children } = props;
+  return (
+    <div class="grid grid-rows-holy h-screen">
+      <header class="py-4">
+        <Container>
+          <a class="text-xl font-mono font-bold text-[#0969da]" href="/">
+            [].blog()
+          </a>
+        </Container>
+      </header>
+      <main>
+        <Container>{children}</Container>
+      </main>
+      <footer class="py-4">
+        <Container>
+          <p class="text-center">
+            © Cosmin Cioaclă 2022{" "}
+            <img
+              class="inline align-middle"
+              src="/fresh-logo.svg"
+              width="20px"
+              height="20px"
+              alt="the fresh logo: a sliced lemon dripping with juice"
+            />{" "}
+            Freshly squeezed by a Denosaur{" "}
+            <img
+              class="inline align-middle"
+              src="/deno-logo.svg"
+              width="20px"
+              height="20px"
+              alt="the deno logo: a dinosaur head in the rain"
+            />
+          </p>
+        </Container>
+      </footer>
+    </div>
+  );
+}
--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,12 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        "holy": "auto 1fr auto",
+      },
+    },
+  },
 } as Options;
