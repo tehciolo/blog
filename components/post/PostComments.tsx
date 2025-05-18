e40d6ede3dd6e3b4acb81322694e879f22b7d877
Cosmin Cioaclă <tehciolo@gmail.com>
2025-05-18 16:54:20 +0200

    docs: explain error

--- components/post/PostComments.tsx
+++ components/post/PostComments.tsx
@@ -2,6 +2,7 @@ export const PostComments = () => {
   return (
     <script
       src="https://utteranc.es/client.js"
+      // @ts-ignore utterances expects non-standard attributes
       repo="tehciolo/blog"
       issue-term="pathname"
       label="utterances"
